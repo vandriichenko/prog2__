9def5b6be1997a6b83b2edb778ade4854d1a11a3
vandriichenko <victoria.skrypkina@gmail.com>
2022-07-14 15:21:36 +0300

    Create CB.cs

---
+++ Lab_1/Lab_4/CB.cs
@@ -0,0 +1,54 @@
+﻿namespace Lab_4
+{
+    public class CB
+    {
+        public string _text { get; set; }
+        // конструктор за умовчанням
+        public CB()
+        {
+
+        }
+
+        // конструктор з параметрами
+        public CB(string text)
+        {
+            _text = text;
+        }
+
+        // конструктор копіювання
+        public CB(CB cb)
+        {
+            _text = cb._text;
+        }
+
+        // метод обчислення довжини рядка
+        public int Length()
+        {
+            return _text.Length;
+        }
+
+        // методи отримання даних рядка
+        public override string ToString()
+        {
+            return _text;
+        }
+
+        // перевантаження оператора +
+        public static CB operator +(CB a, CB b)
+        {
+            return new CB(a._text + b._text);
+        }
+
+        // перевантаження оператора *
+        public static CB operator *(CB a, int count)
+        {
+            var text = "";
+            for (int i = count; i > 0; i--)
+            {
+                text = text + a._text;
+            }
+            return new CB(text);
+        }
+
+    }
+}
