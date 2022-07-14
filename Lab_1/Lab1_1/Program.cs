bad43c69ca5861859d31ab332ff46db3908ac7d4
vandriichenko <victoria.skrypkina@gmail.com>
2022-07-14 15:20:50 +0300

    Create Program.cs

---
+++ Lab_1/Lab1_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab_1_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int A, B;
+            Console.Write("Enter value: ");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            B = A + 1; // +1
+            Console.Out.Write("Result: ");
+            Console.Out.Write(Convert.ToString(A, 10));
+            Console.Out.Write("=>");
+            Console.Out.Write(Convert.ToString(A, 2));
+            Console.Out.Write("=>");
+            Console.Out.Write(Convert.ToString(B, 2));
+            Console.Out.Write("=>");
+            Console.Out.WriteLine(Convert.ToString(B, 10));
+
+        }
+    }
+}
