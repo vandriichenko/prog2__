0901080cc0e478fd6772dc6111c00358ed00a1d6
vandriichenko <victoria.skrypkina@gmail.com>
2022-07-14 15:21:14 +0300

    Create MyContainer.cs

---
+++ Lab_1/Lab_2/MyContainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_2
+{
+    public class MyContainer
+    {
+        private List<MyString> _strings;
+
+        public MyContainer()
+        {
+            _strings = new List<MyString>();
+        }
+
+        public MyContainer add(MyString one)
+        {
+            _strings.Add(one);
+            return this;
+        }
+
+        public void Show()
+        {
+            Console.Out.WriteLine();
+            foreach (var str in _strings)
+            {
+                Console.Out.WriteLine(str);
+            }
+        }
+    }
+}
