0b07528e02e6ef0963a4c9585afc38dd707e045f
vandriichenko <victoria.skrypkina@gmail.com>
2022-07-14 15:21:50 +0300

    Create LetterCB.cs

---
+++ Lab_1/Lab_5/LetterCB.cs
@@ -0,0 +1,17 @@
+﻿using Lab_4;
+
+namespace Lab_5
+{
+    public class LetterCB : CB
+    {
+        public LetterCB(string text) : base(text)
+        {
+        }
+
+        public LetterCB shift()
+        {
+            _text = _text[_text.Length - 1] + _text.Substring(1, _text.Length - 2);
+            return this;
+        }
+    }
+}
