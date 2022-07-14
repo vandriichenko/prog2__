d864dd218e4c6c11965d2f86a5ea95e6571b3e95
vandriichenko <victoria.skrypkina@gmail.com>
2022-07-14 15:21:20 +0300

    Create MyString.cs

---
+++ Lab_1/Lab_2/MyString.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab_2
+{
+    public class MyString
+    {
+        private string _text;
+
+        public MyString(string text)
+        {
+            this._text = text;
+        }
+
+        public MyString Concat(string text)
+        {
+            _text = _text + text;
+            return this;
+        }
+
+        public MyString Replace(string subtext, string on)
+        {
+            _text = _text.Replace(subtext, on);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return _text.ToString();
+        }
+    }
+}
