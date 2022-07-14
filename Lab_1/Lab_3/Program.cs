c843f9f2a305f01b6812321690d6f7ea91297067
vandriichenko <victoria.skrypkina@gmail.com>
2022-07-14 15:21:32 +0300

    Create Program.cs

---
+++ Lab_1/Lab_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab_3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Клас інкапсулює двовимірний масив з елементів символьного типу.
+            // Створити одновимірний індексатор, що повертає рядок, який
+            // складається з усіх елементів однієї з 2-х діагоналей. Створити
+            // властивість, що контролює доступ до змінної, яка містить кількість
+            // голосних літер у масиві
+            var myArray = new MyArray(new char[3, 3] { { 'a', 'b', 'z' }, { 'a', '5', '2' }, { 'b', '+', '#' } });
+            Console.WriteLine(myArray[0]);
+            Console.WriteLine(myArray[1]);
+            Console.WriteLine(myArray.CountVowels);
+            myArray.CountVowels = 5;
+
+        }
+    }
+}
