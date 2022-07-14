c550c51c4c4124fd5c67ee2b8f2aa0a74c6421ed
vandriichenko <victoria.skrypkina@gmail.com>
2022-07-14 15:21:23 +0300

    Create Program.cs

---
+++ Lab_1/Lab_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab_2
+{
+    internal class Program
+    {
+        /* Клас-рядок, який містить методи, необхідні для роботи методів класу-контейнера.
+           Клас-контейнер, який є абстракцією тексту та складається з 
+            об’єктів класу-рядка та 
+            методів додавання рядка до тексту, 
+            видалення рядка з тексту, 
+            заміни вказаного рядка заданим рядком, 
+            очищення тексту, повернення кількості рядків, 
+            повернення рядка з символів-цифр, що трапляються в тексті */
+        public static void Main(string[] args)
+        {
+            var myString01 = new MyString("Hello");
+            Console.Out.Write("Рядок: " +
+                              myString01
+                                  .Concat(", World")
+                              );
+
+            var myString02 = new MyString("Hello");
+
+            Console.Out.Write("Рядок: " +
+                              myString02
+                                  .Concat(", World")
+                                  .Replace("World", "Ukraine")
+                              );
+
+            var container = new MyContainer()
+                .add(myString01)
+                .add(myString02);
+            container.Show();
+
+        }
+    }
+}
+
